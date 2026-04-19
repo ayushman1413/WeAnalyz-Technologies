8c16208f735a0770eafadffc6aebf7c4bcd8c418
ayushman1413 <ayushman1413@gmail.com>
2026-04-19 23:58:05 +0530

    update

--- contexts/ResponsiveContext.jsx
+++ contexts/ResponsiveContext.jsx
@@ -10,9 +10,6 @@ export const ResponsiveProvider = ({ children }) => {
     return 1024;
   });
 
-
-
-  
   useEffect(() => {
     const handleResize = () => setScreenWidth(window.innerWidth);
     window.addEventListener('resize', handleResize);
